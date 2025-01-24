2eef0dbf3e8f31517395c203bccfd6d030ff0f79
syed samad ali <samadali0125@gmail.com>
2025-01-24 16:50:53 +0530

    Access Modifiers in Java

--- Users/samad ali/Desktop/Java-Core/methods/test.java
+++ Users/samad ali/Desktop/Java-Core/methods/test.java
@@ -3,19 +3,23 @@ package methods;
 public class test {
      public static void main(String[] args) {
         //  printjunk2(); // call static methods in every static methods as well as non static methods
-        // printjunk(); // cannot call instance methods in static methods
+        // printjunk(); // cannot call instance(non-static) methods in static methods
      }
 
      public void printjunk(){
         System.out.println("Hello, world!");
          printjunk2(); // call static methods in non static methods also
+         junk3();// call non static methods in non static methods also
+
      }
 
      public static void printjunk2(){
         System.out.println("Hello, world Again!");
 
      }
-
+     public void junk3(){
+      System.out.println("Hello world 3");
+     }
 
      // two types of methods :
      // 1. static methods : call  in every static methods and non static methods
